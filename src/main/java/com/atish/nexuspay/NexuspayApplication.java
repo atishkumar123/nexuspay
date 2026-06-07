029736e5dc24591324497cbf1c038d4d4d88393d
atish.k <atishkumar12548@gmail.com>
2026-06-07 21:29:00 +0530

    initial commit

---
+++ src/main/java/com/atish/nexuspay/NexuspayApplication.java
@@ -0,0 +1,13 @@
+package com.atish.nexuspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuspayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuspayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atish/nexuspay/NexuspayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atish.nexuspay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuspayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
